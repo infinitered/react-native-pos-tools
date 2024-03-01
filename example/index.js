d317f21cbfdb28abfbf685b65f86dbb04aa07014
Joshua Yoes <37849890+joshuayoes@users.noreply.github.com>
2024-03-01 13:35:49 -0800

    fix(index.js): add AppRegistry

--- example/index.js
+++ example/index.js
@@ -1,1 +1,7 @@
-export { default } from './src/App';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+export default App
